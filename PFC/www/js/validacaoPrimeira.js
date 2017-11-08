3718a952f8c00b53114bbc1cbe513dc26de1da2b
IvesCruz <ivescruzz@gmail.com>
2017-11-08 11:08:09 -0300

    08-11

--- PFC/www/js/validacaoPrimeira.js
+++ PFC/www/js/validacaoPrimeira.js
@@ -18,30 +18,18 @@ function validacao(){
 			alert("Preencha o campo COMPRIMENTO.")
 			document.dados.tx_comprimento.focus();
 			return false;
-		}else if(isNaN(dados.tx_comprimento.value)){
-			alert("Preencha o campo COMPRIMENTO apenas com NÚMEROS.")
-			document.dados.tx_comprimento.focus();
-			return false;
-		}   
+		}
 
 		//largura
 		if (document.dados.tx_largura.value == ""){
 			alert("Preencha o campo LARGURA.")
 			document.dados.tx_largura.focus();
 			return false;
-		}else if(isNaN(dados.tx_largura.value)){
-			alert("Preencha o campo LARGURA apenas com NÚMEROS.")
-			document.dados.tx_largura.focus();
-			return false;
-		}   
+		}
 
 		//altura
 		if (document.dados.tx_altura.value == ""){
 			alert("Preencha o campo ALTURA.")
-			document.dados.tx_altura.focus();
-			return false;
-		}else if(isNaN(dados.tx_altura.value)){
-			alert("Preencha o campo ALTURA apenas com NÚMEROS.")
 			document.dados.tx_altura.focus();
 			return false;
 		}
@@ -49,10 +37,6 @@ function validacao(){
 		//Temperatura Interna
 		if (document.dados.tx_TempInterna.value == ""){
 			alert("Preencha o campo TEMPERATURA INTERNA.")
-			document.dados.tx_TempInterna.focus();
-			return false;
-		}else if(isNaN(dados.tx_TempInterna.value)){
-			alert("Preencha o campo TEMPERATURA INTERNA apenas com NÚMEROS.")
 			document.dados.tx_TempInterna.focus();
 			return false;
 		}
@@ -75,10 +59,6 @@ function validacao(){
 			alert("Preencha o campo ESPESSURA DO ISOLAMENTO.")
 			document.dados.tx_EspessuraIsolamento.focus();
 			return false;
-		}else if(isNaN(dados.tx_EspessuraIsolamento.value)){
-			alert("Preencha o campo ESPESSURA DO ISOLAMENTO apenas com NÚMEROS.")
-			document.dados.tx_EspessuraIsolamento.focus();
-			return false;
 		}
 
 		//Temperatura Externa
@@ -86,19 +66,11 @@ function validacao(){
 			alert("Preencha o campo TEMPERATURA EXTERNA.")
 			document.dados.tx_TempExterna.focus();
 			return false;
-		}else if(isNaN(dados.tx_TempExterna.value)){
-			alert("Preencha o campo TEMPERATURA EXTERNA apenas com NÚMEROS.")
-			document.dados.tx_TempExterna.focus();
-			return false;
 		}
 
 		//Frequência
 		if (document.dados.tx_Frequencia.value == ""){
 			alert("Preencha o campo FREQUÊNCIA.")
-			document.dados.tx_Frequencia.focus();
-			return false;
-		}else if(isNaN(dados.tx_Frequencia.value)){
-			alert("Preencha o campo FREQUÊNCIA apenas com NÚMEROS.")
 			document.dados.tx_Frequencia.focus();
 			return false;
 		}
--- PFC/www/js/validacaoSegunda.js
+++ PFC/www/js/validacaoSegunda.js
@@ -5,19 +5,11 @@ function validacao(){
 		alert("Preencha o campo MASSA TOTAL DE PRODUTO ARMAZENADO.")
 		document.dados.tx_MassaTotalArma.focus();
 		return false;
-	}else if(isNaN(dados.tx_MassaTotalArma.value)){
-		alert("Preencha o campo MASSA TOTAL DE PRODUTO ARMAZENADO apenas com NÚMEROS.")
-		document.dados.tx_MassaTotalArma.focus();
-		return false;
 	}
 
 	//Massa de Produto em Movimento
 	if (document.dados.tx_MassaProdMov.value == ""){
 		alert("Preencha o campo MASSA DE PRODUTO EM MOVIMENTO.")
-		document.dados.tx_MassaProdMov.focus();
-		return false;
-	}else if(isNaN(dados.tx_MassaProdMov.value)){
-		alert("Preencha o campo MASSA DE PRODUTO EM MOVIMENTO apenas com NÚMEROS.")
 		document.dados.tx_MassaProdMov.focus();
 		return false;
 	}
@@ -27,19 +19,11 @@ function validacao(){
 		alert("Preencha o campo TEMPO DE RESFRIAMENTO E/OU CONGELAMENTO DO PRODUTO.")
 		document.dados.tx_TempResfCongProd.focus();
 		return false;
-	}else if(isNaN(dados.tx_TempResfCongProd.value)){
-		alert("Preencha o campo TEMPO DE RESFRIAMENTO E/OU CONGELAMENTO DO PRODUTO apenas com NÚMEROS.")
-		document.dados.tx_TempResfCongProd.focus();
-		return false;
 	}
 
 	//Temperatura de Entrada do Produto
 	if (document.dados.tx_TempEntrada.value == ""){
 		alert("Preencha o campo TEMPERATURA DE ENTRADA DO PRODUTO.")
-		document.dados.tx_TempEntrada.focus();
-		return false;
-	}else if(isNaN(dados.tx_TempEntrada.value)){
-		alert("Preencha o campo TEMPERATURA DE ENTRADA DO PRODUTO apenas com NÚMEROS.")
 		document.dados.tx_TempEntrada.focus();
 		return false;
 	}
